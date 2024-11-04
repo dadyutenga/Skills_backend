12f48d280095c0814f5004854cea1f40b8f056f7
Your Name <you@example.com>
2024-11-04 16:03:28 +0300

    push 111

--- Skills_backend/settings.py
+++ Skills_backend/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Add template directory configuration
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -46,7 +50,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'django_filters',
     'channels',
-    
+    'authlib',
 ]
 
 MIDDLEWARE = [
@@ -139,4 +143,13 @@ GRAPHENE = {
 GRAPHQL_JWT = {
     'JWT_VERIFY_EXPIRATION': True,
 }
+
+# Add Auth0 settings at the end of the file
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
+AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
+AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
 
